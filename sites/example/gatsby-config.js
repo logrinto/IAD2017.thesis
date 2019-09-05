8499f6789864876a0a79f831f3e823d49478c224
Stefan Huber <sh@signalwerk.ch>
2019-09-05 02:45:45 +0200

    ADD: siteUrl

--- sites/example/gatsby-config.js
+++ sites/example/gatsby-config.js
@@ -1,17 +1,20 @@
 module.exports = {
-  pathPrefix: `/blog`,
+  pathPrefix: `/IAD2017.thesis`,
 
-  __experimentalThemes: [{
-    resolve: 'gatsby-theme-signalwerk',
-    options: {
-      root: __dirname,
+  __experimentalThemes: [
+    {
+      resolve: "gatsby-theme-signalwerk",
+      options: {
+        root: __dirname
+      }
     }
-  }],
+  ],
 
   siteMetadata: {
-    title: 'Thesis IAD2017',
-    author: 'Schule für Gestaltung Zürich',
-    authorUrl: 'http://signalwerk.ch/',
-    description: 'A starter blog ...'
-  },
+    title: "Thesis IAD2017",
+    author: "Schule für Gestaltung Zürich",
+    authorUrl: "http://signalwerk.ch/",
+    description: "A starter blog ...",
+    siteUrl: "https://thesis-iad2017.logrinto.ch"
+  }
 };
