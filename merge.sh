dbd8e6ae07f0226706be3448eb14ef83d482df45
signalwerk <sh@signalwerk.ch>
2020-01-08 09:17:26 +0100

    FIX: merge

--- merge.sh
+++ merge.sh
@@ -20,6 +20,5 @@ git merge remotes/origin/natasha --no-edit
 
 # git merge remotes/origin/Marc --no-edit
 # git merge remotes/origin/marius --no-edit
-# git merge remotes/origin/master --no-edit
 # git merge remotes/origin/tieukhe --no-edit
 # git merge remotes/origin/wolfgang --no-edit
