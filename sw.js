4c2785c7150429ad025b54b9b1dfc47c264596e8
CI System <sh@signalwerk.ch>
2020-01-08 18:03:43 +0000

    Deploy to GitHub Pages: f57d06e8315fd2403a236d414302d91331b56ce0

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-caed186f32efceac1d67.js"
+    "url": "webpack-runtime-f1a8ae3bff96de371824.js"
   },
   {
     "url": "styles.f6e0e42109ac584c39d1.css"
@@ -39,14 +39,14 @@ self.__precacheManifest = [
     "url": "commons-1a82adb5dc4d48454d59.js"
   },
   {
-    "url": "app-1284c27cd46fd70558db.js"
+    "url": "app-47a5542ea3a19cbc1ce7.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-01d81ee96d50b3ca8b0f.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "793080bd80c1a70afaaed3fe0aecd57c"
+    "revision": "3061b26e29fa37c348235b22e046da3f"
   },
   {
     "url": "manifest.webmanifest",
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-1284c27cd46fd70558db.js`))) {
+  if (!resources || !(await caches.match(`/app-47a5542ea3a19cbc1ce7.js`))) {
     return await fetch(event.request)
   }
 
