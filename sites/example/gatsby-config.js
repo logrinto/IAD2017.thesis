0f9332b6676e1eb6a272ef96ca82138437beb964
Stefan Huber <sh@signalwerk.ch>
2020-01-18 18:24:32 +0100

    Update gatsby-config.js

--- sites/example/gatsby-config.js
+++ sites/example/gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     title: "Thesis IAD2017",
     author: "Schule für Gestaltung Zürich",
     authorUrl: "https://sfgz.ch/",
-    description: "A starter blog ...",
+    description: "Thesis-Arbeiten der HF Interaction Design Lehrgang 2017",
     siteUrl: "https://thesis.logrinto.ch"
   }
 };
